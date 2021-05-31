094f12db7a11208d67febc9815e1479fe710891d
Lovisa Bohman <lovisabo@hotmail.com>
2021-05-31 13:15:56 +0200

    Create the home-component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { GetBlogsService } from 'src/app/services/get-blogs.service';
+import { Blog } from '../../models/Blog';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  public blogs: Blog[] = [];
+  constructor(private blogService: GetBlogsService) { }
+
+  ngOnInit(): void {
+    this.blogService.getBlogs().subscribe(blog => {
+      this.blogs = blog;
+    });
+  }
+
+}
