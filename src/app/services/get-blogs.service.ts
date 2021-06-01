ee43ac3c0e019800f4413cacd5db366795891dc5
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-01 20:47:52 +0200

    Add more functionality to the getBlogsService, ability to save to localStorage.

--- src/app/services/get-blogs.service.ts
+++ src/app/services/get-blogs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Blog } from '../models/Blog';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,22 @@ import { Observable } from 'rxjs';
 //En service som hämtar alla bloggar tillhörande id: 5020
 export class GetBlogsService {
 
+  private blogs: Blog[] = [];
   constructor(private httpCall: HttpClient) { }
 
   getBlogs(): Observable<Blog[]> {
     let userId = 5020;
 
-    return this.httpCall.get<Blog[]>('https://mi-blogs.azurewebsites.net/api/Blogs/user/' + userId);
+    if(!localStorage.getItem('blogs')) {
+      //Hämta från API:et och spara i LS
+      this.httpCall.get<Blog[]>('https://mi-blogs.azurewebsites.net/api/Blogs/user/' + userId).subscribe((data) => {
+        this.blogs = data;
+        localStorage.setItem('blogs', JSON.stringify(data));
+      });
+    } else {
+      //Hämta data från LS
+      this.blogs = JSON.parse(localStorage.getItem('blogs'));
+    }
+    return of(this.blogs);
   }
 }
