2ec885918b8c1e93d0f81b08a1d3f8c80c195a98
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-03 17:36:17 +0200

    -

--- src/app/components/create-new-blog/create-new-blog.component.ts
+++ src/app/components/create-new-blog/create-new-blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Blog } from 'src/app/models/Blog';
+import { CreateNewBlogService } from 'src/app/services/create-new-blog.service';
 
 @Component({
   selector: 'app-create-new-blog',
@@ -9,11 +10,16 @@ import { Blog } from 'src/app/models/Blog';
 })
 export class CreateNewBlogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private blogService: CreateNewBlogService) { }
 
   title = new FormControl('');
   
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  // createNewBlog() {
+  //   let newBlog = new Blog(this.title.value, new Date());
+  //   this.blogService.postNewBlog(newBlog).subscribe.((data) => {
+  //     this.blogs.push((data));
+  //   })
+  // }
 }
