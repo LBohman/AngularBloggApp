95f317eac8444b08ca22a4151a39f5aaaa08b349
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-03 13:14:24 +0200

    Remove some extra spaces in the code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
-// import { BlogComponent } from './components/blog/blog.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
