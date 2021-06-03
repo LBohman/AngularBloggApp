54087ca1bd119f06d62388cab11c24c4e1afc2b0
Lovisa Bohman <lovisabo@hotmail.com>
2021-06-03 13:28:10 +0200

    Add comments to clarify what happens in the code

--- src/app/components/blog-detail/blog-detail.component.ts
+++ src/app/components/blog-detail/blog-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Blog } from 'src/app/models/Blog';
+import { GetBlogsService } from 'src/app/services/get-blogs.service';
 
 @Component({
   selector: 'app-blog-detail',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-detail.component.scss']
 })
 export class BlogDetailComponent implements OnInit {
+  //Lagrar parametervärdet id
+  id: number = 0;
+  //Skapar ett värde blog, med datatypen Blog
+  blog: Blog;
 
-  constructor() { }
+  //Hämta parametern från url:en och ger tillgång till vår tjänst
+  constructor(private route: ActivatedRoute, private service: GetBlogsService) { }
 
   ngOnInit(): void {
+    //Tittar på parametern i url:en, vilket kommer vara id:et som är ett tal
+    this.route.paramMap.subscribe((params) => {
+      this.id = parseInt(params.get('id'));
+
+      //Via tjänsten använda id:et för att visa den specifika bloggen som önskas
+      this.blog = this.service.getBlog(this.id);
+    });
   }
 
 }
