1d94b2ba46de924d2e7ac0a4be89591ef176f83c
Lovisa Bohman <lovisabo@hotmail.com>
2022-03-08 22:41:53 +0100

    Add button to navigate to url of /create-new-blog from homepage.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { GetBlogsService } from 'src/app/services/get-blogs.service';
 import { Blog } from '../../models/Blog';
 
@@ -9,13 +10,17 @@ import { Blog } from '../../models/Blog';
 })
 export class HomeComponent implements OnInit {
   public blogs: Blog[] = [];
-  constructor(private blogService: GetBlogsService) { }
+  constructor(private blogService: GetBlogsService, private router: Router) { }
 
   ngOnInit(): void {
     this.blogService.blogs$.subscribe(data => {
       this.blogs = data;
     });
     this.blogService.getBlogs();
+  }
+
+  navigateCreateBlog(): void {
+    this.router.navigate(['/create-new-blog']);
   }
 
 }
