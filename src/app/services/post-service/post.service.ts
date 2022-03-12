aaab94f5d1fce870c0ee621afa63ff932ae36fe3
Lovisa Bohman <lovisabo@hotmail.com>
2022-03-12 16:02:12 +0100

    The createNewPost now actually posts. Amazing.

--- src/app/services/post-service/post.service.ts
+++ src/app/services/post-service/post.service.ts
@@ -1,14 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Blog } from 'src/app/models/Blog';
+import { Post } from 'src/app/models/Post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  private posts = new Subject<Post[]>();
+  posts$ = this.posts.asObservable();
 
   constructor(private http: HttpClient) { }
 
-  
+  getPost(postId: number): Observable<Post> {
+    return this.http.get<Post>('https://mi-blogs.azurewebsites.net/api/Posts' + postId);
+  }
+
+  createNewPost(post: Post): Observable<Post> {
+    return this.http.post<Post>('https://mi-blogs.azurewebsites.net/api/Posts/', post);
+  }
 }
