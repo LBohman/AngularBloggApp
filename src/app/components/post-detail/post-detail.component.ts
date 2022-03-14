2755e9ef97590103e3be4b000b8ac54137a93b34
Lovisa Bohman <lovisabo@hotmail.com>
2022-03-14 17:15:06 +0100

    Display the post and navigation to get to previous page with the list of posts.

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Comment } from 'src/app/models/Comment';
+import { Post } from 'src/app/models/Post';
+import { PostService } from 'src/app/services/post-service/post.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-detail.component.scss']
 })
 export class PostDetailComponent implements OnInit {
+  id: number = 0;
+  post: Post;
+  comments: Comment[];
 
-  constructor() { }
+  constructor(private postService: PostService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.id = parseInt(params.get('id'));
+      this.postService.getPost(this.id).subscribe((post) => {
+        this.post = post;
+        console.log(this.post);
+        this.comments = post.comments;
+      });
+    })
+  }
+
+  backToAllPosts(): void {
+    this.router.navigate(['/blog/' + this.post.blogId]);
   }
 
 }
